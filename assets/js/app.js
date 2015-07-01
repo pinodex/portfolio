8c7fd30560ec052d570885a2cd83b31e1a7b3e9a
Raph Marco <raphmarco@hotmail.ph>
2015-07-01 18:46:37 +0800

    Adding some stuffs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-(function() {
+//(function() {
 
     "use strict";
 
@@ -46,6 +46,109 @@
         }, 80);
     }
 
-    type();
+    if (target) {
+        type();
+    }
 
-})();
+    var email;
+    
+    if (email = document.querySelector('[data-e]')) {
+        email.innerHTML = window.atob(email.getAttribute('data-e'));
+    }
+
+    var wordPool;
+    var wordPoolStyle;
+    var words = [];
+    var currentWord = 0;
+    var threshold = 50;
+    var currentThreshold = 0;
+
+    if (wordPool = document.querySelector('ul.word-pool')) {
+        var wordCount = wordPool.childNodes.length;
+        var zIndex = 1;
+
+        while (wordCount--) {
+            if (wordPool.childNodes[wordCount].nodeType == 1) {
+                var wordEl = wordPool.childNodes[wordCount];
+
+                wordEl.style.marginTop = '-' + (wordEl.clientHeight / 2) + 'px';
+                wordEl.style.marginLeft = '-' + (wordEl.clientWidth / 2) + 'px';
+                wordEl.style.zIndex = zIndex++;
+                wordEl.style.opacity = '0';
+
+                words.unshift(wordPool.childNodes[wordCount]);
+            }
+        }
+
+        wordPoolStyle = window.getComputedStyle(wordPool);
+
+        animateWordPool();
+        reposWords();
+    }
+
+    function animateWordPool() {
+        var wordEl = words[currentWord];
+        var elStyle = window.getComputedStyle(wordEl);
+
+        if (currentThreshold++ >= threshold) {
+            currentThreshold = 0;
+            currentWord++;
+
+            if (currentWord == words.length) {
+                currentWord = 0;
+            }
+        }
+
+        if (wordEl.style.opacity < 1) {
+            wordEl.style.opacity = parseFloat(wordEl.style.opacity) + 0.05;
+        }
+
+        setTimeout(animateWordPool, 10);
+    }
+
+    function reposWords() {
+        for (var i = 0; i < words.length; i++) {
+            var wordEl = words[i];
+            var elStyle = window.getComputedStyle(wordEl);
+            var elFontSize = parseFloat(elStyle.fontSize);
+
+            if (elFontSize < 48 && wordEl.style.opacity < 0.2) {
+                continue;
+            }
+
+            if (elFontSize < 48) {
+                wordEl.style.fontSize = (elFontSize + 0.2) + 'px';
+            }
+
+            if (elFontSize > 48) {
+                wordEl.style.opacity = parseFloat(wordEl.style.opacity) - 0.05;
+            }
+
+            var posTopOffset = parseFloat(wordPoolStyle.height) - parseFloat(elStyle.top);
+
+            if (posTopOffset < 0 || posTopOffset > 200) {
+                wordEl.style.opacity = 0;
+                wordEl.style.fontSize = '16px';
+                wordEl.style.top = '50%';
+                wordEl.style.left = '50%';
+            }
+
+            wordEl.style.marginTop = '-' + (wordEl.clientHeight / 2) + 'px';
+            wordEl.style.marginLeft = '-' + (wordEl.clientWidth / 2) + 'px';
+
+            var posTop = parseFloat(elStyle.top);
+            var posLeft = parseFloat(elStyle.left);
+
+            if (i % 2 == 0) {
+                wordEl.style.top = (posTop + 0.5) + 'px';
+                wordEl.style.left = (posLeft + 0.5) + 'px';
+            } else {
+                wordEl.style.top = (posTop - 0.5) + 'px';
+                wordEl.style.left = (posLeft - 0.5) + 'px';
+            }
+        };
+
+        setTimeout(reposWords, 20);
+    }
+
+//})();

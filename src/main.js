71b0528d85cc20ebb3c7a53fd89451d57510c17e
Raphael Marco <raphmarco@hotmail.ph>
2019-04-21 01:00:55 +0800

    Let there be light...

--- src/main.js
+++ src/main.js
@@ -1,48 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-
-import 'es6-promise/auto'
-
 import Vue from 'vue'
-import VueProgressBar from 'vue-progressbar'
-import VueAnalytics from 'vue-analytics'
-
-import App from './App'
-import Browser from './components/Browser'
-import Screen from './components/Screen'
-
+import App from './App.vue'
 import router from './router'
-import services from './services'
-
-import 'bulma'
 
 Vue.config.productionTip = false
 
-Vue.use(VueProgressBar, {
-  color: '#607d8b',
-  failedColor: 'red',
-  height: '1px'
-})
-
-Vue.use(VueAnalytics, {
-  id: __GA_CODE__, router
-})
-
-Vue.component('Browser', Browser)
-Vue.component('Screen', Screen)
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  template: '<App/>',
-  components: { App },
-
-  provide: services,
-
-  methods: {
-    setTitle (text) {
-      document.title = `${text} - Raphael Marco`
-    }
-  }
+  el: '#app',
+  render: h => h(App)
 })
--- src/works/sweetfm.md
+++
@@ -1,5 +0,0 @@
-_This project is now defunct._
-
-The website features a chat system with Facebook as the user provider, parsing of current song playing in a SHOUTcast server, and fetching of song album art. The website uses HTML5 audio player API to play the MP3 stream from a SHOUTcast server.
-
-The website was developed with **Silex Framework** and **Foundation Framework**.

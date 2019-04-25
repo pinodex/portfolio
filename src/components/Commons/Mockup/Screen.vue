24f40867684dbcf9e6766915b15a1c22256aad8c
Raphael Marco <raphmarco@hotmail.ph>
2019-04-26 03:54:07 +0800

    Fix mockup box-shadow

--- src/components/Commons/Mockup/Screen.vue
+++ src/components/Commons/Mockup/Screen.vue
@@ -45,8 +45,7 @@ export default {
 
   position: relative;
 
-  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1),
-              0 0 0 1px rgba(10, 10, 10, 0.1);
+  box-shadow: 0 5px 10px 0px rgba(0, 0, 0, 0.10);
 }
 
 svg {

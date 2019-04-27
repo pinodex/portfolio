ddeb002c30d607708c356d74f0c295c8b170637c
Raphael Marco <raphmarco@hotmail.ph>
2019-04-27 19:40:00 +0800

    Add story page

---
+++ data/story.md
@@ -0,0 +1,9 @@
+# Storytime
+
+Ever since I&lsquo;ve been a tinkerer. I like knowing how things work. I&lsquo; always interested in how the world works and how it is powered by technology.
+
+I started coding when I was 11 years old. I always wanted to build something to be used by other people. It is my encouragement for learning and building stuffs, that people can benefit from my works.
+
+From there, I&lsquo;ve worked with different projects, sometimes alone, sometimes with other people and volunteered to do websites and systems for the purpose of learning. I learn by doing actual things.
+
+I put the label **Full-Stack Developer** under my name, having the knowledge and experience I gained doing different projects.
--- src/pages/Story.vue
+++ src/pages/Story.vue
@@ -1,17 +1,44 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <div class="hero is-medium is-primary is-bold has-border-radius">
-        <div class="hero-body">
+  <div>
+    <section class="section">
+      <div class="container">
+        <div class="hero is-primary is-bold has-border-radius">
+          <div class="hero-body">
+            <figure class="image is-128x128 is-element-centered has-contents-below">
+              <img class="is-rounded" src="/assets/img/avatar.jpg" alt="Avatar">
+            </figure>
 
+            <div class="has-text-centered">
+              <h1 class="is-size-3">Raphael Marco</h1>
+              <h2 class="is-size-6">Full-Stack Developer</h2>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-8">
+            <div class="content">
+              <component :is="story"></component>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import story from '@data/story.md'
+
 export default {
+  computed: {
+    story: () => story
+  },
+
   mounted () {
     this.setPageTitle('Story')
   }

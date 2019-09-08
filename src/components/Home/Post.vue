f5c1f2c15d55e55644667a52c15a65a64a811263
Raphael Marco <raphaelmarco@outlook.com>
2019-09-09 01:50:56 +0800

    Remove unused blog code

--- src/components/Home/Post.vue
+++ src/components/Home/Post.vue
@@ -22,8 +22,6 @@
 </template>
 
 <script>
-import { blog } from '@/config'
-
 export default {
   props: {
     post: {
--- src/config.js
+++ src/config.js
@@ -13,12 +13,5 @@ module.exports = {
     }
   },
 
-  blog: {
-    url: process.env.VUE_APP_BLOG_URL,
-    apiBase: process.env.VUE_APP_BLOG_API_BASE,
-    clientId: process.env.VUE_APP_BLOG_CLIENT_ID,
-    clientSecret: process.env.VUE_APP_BLOG_CLIENT_SECRET
-  },
-
   googleAnalyticsId: process.env.VUE_APP_GA_CODE
 }
